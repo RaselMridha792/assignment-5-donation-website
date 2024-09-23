788088b794a4c0ef1468e16d611ddf59a58f8c76
rasel mridha <raselmridha792@gmail.com>
2024-09-23 06:34:16 +0600

    added time and date in history section

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ document.getElementById('donate-amount-btn').addEventListener('click', function(
     const totalFunds = getInputFromDiv('increment-amount');
     const accountBalance = getInputFromDiv('available-balance');
     if(donatedInput > 0){
+
+        // check the donated amount less or grater then the donated amount 
         if(donatedInput< accountBalance){
             const newFunds = totalFunds + donatedInput;
             document.getElementById('increment-amount').innerText = newFunds;
@@ -14,6 +16,13 @@ document.getElementById('donate-amount-btn').addEventListener('click', function(
     
             const availableBalance = accountBalance - donatedInput;
             document.getElementById('available-balance').innerText = availableBalance;
+            // added modal when click button 
+            my_modal_1.showModal();
+            // update the modal amount balance 
+            document.getElementById('modal-updated-amount').innerText = `${donatedInput} BDT`;
+            const currentDate = new Date();
+            donationHistory(donatedInput, currentDate);
+
         }
         else{
             alert('sorry! not enough money in your account. please contact your bank');
@@ -27,4 +36,18 @@ document.getElementById('donate-amount-btn').addEventListener('click', function(
         alert('sorry! negative number. please enter the right amount for the donation.');
     }
     
+})
+
+
+document.getElementById('donation-page').addEventListener('click', function(){
+    // go to the page 
+    buttonAction('card-section');
+    // change the bg color of donation page btn 
+    changeBtnColor('donation-page', 'history-page');
+})
+document.getElementById('history-page').addEventListener('click', function(){
+    // go to the page 
+    buttonAction('history-section');
+    changeBtnColor('history-page', 'donation-page');
+
 })
--- js/multi-functions.js
+++ js/multi-functions.js
@@ -16,3 +16,35 @@ function getInputFromDiv(id){
     const incrementAmountNumber = parseFloat(incrementAmount);
     return incrementAmountNumber;
 }
+// for adding history section 
+
+// show or hidden pages 
+function buttonAction(id){
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('card-section').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+    
+}
+
+
+
+function donationHistory(id, date) {
+    const historyOfDonation = document.createElement('div'); // Use 'div' as a valid HTML element
+    historyOfDonation.innerHTML = `
+<div class="border rounded-xl shadow-lg p-8 my-10">
+    <h1 class="font-bold text-2xl mb-2">${id} Taka is Donated for famine-2024 at Feni, Bangladesh</h1>
+    <p class="text-gray-500 font-normal text-lg">Date : ${date}</p>
+</div>
+`;
+    document.getElementById('donation-history').appendChild(historyOfDonation);
+}
+
+
+function changeBtnColor(firstBtn, secondBtn){
+    document.getElementById(firstBtn).classList.add('bg-[#B4F461]');
+    document.getElementById(firstBtn).classList.remove('bg-white');
+    
+    // reset the bg color in history page btn 
+    document.getElementById(secondBtn).classList.remove('bg-[#B4F461]');
+    document.getElementById(secondBtn).classList.add('bg-white');
+}
